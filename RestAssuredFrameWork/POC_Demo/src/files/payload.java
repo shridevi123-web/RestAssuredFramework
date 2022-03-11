8e89fc66452d9600c21ffa5b501563dd1f750929
shridevi123-web <77652993+shridevi123-web@users.noreply.github.com>
2022-03-11 12:24:13 +1100

    This is only a framework wit a sample java class

---
+++ RestAssuredFrameWork/POC_Demo/src/files/payload.java
@@ -0,0 +1,18 @@
+package files;
+
+public class payload {
+	
+	public static String IDMLoginPayload()
+	{
+		return "{ \r\n"
+				+ "	\"shopperId\": \"02112021\",\r\n"
+				+ "    \"username\": \"savedcarduser\"\r\n"
+				+ "}";
+	}
+
+	public static String CVVTokanizationPayload()
+	{
+		return "";
+	}
+
+}
